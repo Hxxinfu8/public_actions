bfe2675f4cc1ee95d882e4d5ddb817c1deeb72e4
hx_xinfu8 <40448750+Hxxinfu8@users.noreply.github.com>
2024-08-15 16:53:49 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,11 +2,12 @@ const [cookie, user, pass, to] = process.argv.slice(2);
 
 const headers = {
   'content-type': 'application/json; charset=utf-8',
-  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
-  'accept-encoding': 'gzip, deflate, br',
-  'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-  'sec-ch-ua': '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
+  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0',
+  'accept-encoding': 'gzip, deflate, br, zstd',
+  'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+  'sec-ch-ua': '"Not)A;Brand";v="99", "Microsoft Edge";v="127", "Chromium";v="127"',
   'sec-ch-ua-mobile': '?0',
+  'x-ms-token': '8ti4Z8dpk5V_S_EW6vXsCiFGy8JGPaHtDzN5wI5WPn39KX44btqlykVqgna54U4AB4NeQj6C6OGwJm_ffI_LWFzoFch0wN7x5S-FnwG7neoCTH4x-p3TWS7nccIG2_I='
   referer: 'https://juejin.cn/',
   accept: '*/*',
   cookie
