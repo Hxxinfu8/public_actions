96f0dc9f1409c879789d8a389ae629872e705c17
hx_xinfu8 <40448750+Hxxinfu8@users.noreply.github.com>
2024-08-15 16:59:08 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ const headers = {
   'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
   'sec-ch-ua': '"Not)A;Brand";v="99", "Microsoft Edge";v="127", "Chromium";v="127"',
   'sec-ch-ua-mobile': '?0',
-  'x-ms-token': '8ti4Z8dpk5V_S_EW6vXsCiFGy8JGPaHtDzN5wI5WPn39KX44btqlykVqgna54U4AB4NeQj6C6OGwJm_ffI_LWFzoFch0wN7x5S-FnwG7neoCTH4x-p3TWS7nccIG2_I='
+  'x-ms-token': '8ti4Z8dpk5V_S_EW6vXsCiFGy8JGPaHtDzN5wI5WPn39KX44btqlykVqgna54U4AB4NeQj6C6OGwJm_ffI_LWFzoFch0wN7x5S-FnwG7neoCTH4x-p3TWS7nccIG2_I=',
   referer: 'https://juejin.cn/',
   accept: '*/*',
   cookie
